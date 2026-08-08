6ee1d431b1aae36a9652043ecbe906d6ac337cf2
James Nexus - CurveYield DAO <jamesnexus9999@gmail.com>
2026-08-07 22:53:51 -0700

    Delete curveyield-boosthub-live-audit-v1 directory

--- curveyield-boosthub-live-audit-v1/source-versions/standard-v1/fraxtal/interfaces/ISnapshotDelegateRegistry.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
-
-interface ISnapshotDelegateRegistry {
-    function setDelegate(bytes32 id, address delegate) external;
-    function clearDelegate(bytes32 id) external;
-}
